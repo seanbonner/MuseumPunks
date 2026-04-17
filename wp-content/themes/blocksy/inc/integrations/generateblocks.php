b82f30dda7a015d71095ff03670ee61a5aa8a99c
Sean Bonner <960060+seanbonner@users.noreply.github.com>
2026-04-17 12:31:02 -0700

    Remove wp-content; legacy code preserved on wp-legacy branch
    The full WordPress theme and plugin code is archived on the
    wp-legacy branch on origin. Main is now exclusively the Eleventy
    static site.

--- wp-content/themes/blocksy/inc/integrations/generateblocks.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// add_filter('generateblocks_global_css_priority', function ($current) {
-// 	return 55;
-// });
-
-// add_filter('generateblocks_dynamic_css_priority', function ($current) {
-// 	return 60;
-// });
